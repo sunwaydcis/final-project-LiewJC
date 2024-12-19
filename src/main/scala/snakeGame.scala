20800dfe78da8430213b6317f7a15f1257e6a0d9
LiewJC <137489248+LiewJC@users.noreply.github.com>
2024-12-19 16:41:50 +0800

    Update snakeGame.scala

--- src/main/scala/snakeGame.scala
+++ src/main/scala/snakeGame.scala
@@ -25,13 +25,39 @@ class SnakeGame extends Application:
     foodPoint
 
   override def start(primaryStage: Stage): Unit =
-      primaryStage.setTitle("Snake Game - Score: " + score.toString)
-      val root = new StackPane()
-      val canvas = new Canvas(Width, Height)
-      val gc = canvas.getGraphicsContext2D()
-      root.getChildren().add(canvas)
-  
-      val scene = new Scene(root)
-      primaryStage.setScene(scene)
-  
-      primaryStage.show()
+    primaryStage.setTitle("Snake Game - Score: " + score.toString)
+    val root = new StackPane()
+    val canvas = new Canvas(Width, Height)
+    val gc = canvas.getGraphicsContext2D()
+    root.getChildren().add(canvas)
+
+    val scene = new Scene(root)
+    primaryStage.setScene(scene)
+
+    scene.setOnKeyPressed(e =>
+      e.getCode match
+        case KeyCode.UP if direction.y != 1 => direction = new Point(0, -1)
+        case KeyCode.DOWN if direction.y != -1 => direction = new Point(0, 1)
+        case KeyCode.LEFT if direction.x != 1 => direction = new Point(-1, 0)
+        case KeyCode.RIGHT if direction.x != -1 => direction = new Point(1, 0)
+        case _ => ()
+    )
+
+    scene.setOnKeyReleased(e =>
+      e.getCode match
+        case KeyCode.SPACE if gameOver => restartGame(); gameOver = false
+        case _ => ()
+    )
+
+    val timer = new AnimationTimer {
+      override def handle(now: Long): Unit =
+        if !gameOver then
+          frameCounter += 1
+          if frameCounter >= UpdateInterval then
+            update()
+            frameCounter = 0
+          draw(gc)
+          primaryStage.setTitle("Snake Game - Score: " + score.toString)
+    }
+    timer.start()
+    primaryStage.show()
